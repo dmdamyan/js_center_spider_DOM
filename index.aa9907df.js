12459b47f5767e6b6bcd5954f5c8e0ab82641d92
Dmitrii Damyan <dmitriignc@gmail.com>
2024-12-21 21:02:07 +0200

    make build

---
+++ index.aa9907df.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("img.spider"),t=document.querySelector("div, wall");e.style.top=t.clientHeight/2-e.clientHeight/2+"px",e.style.left=t.clientWidth/2-e.clientWidth/2+"px";
+//# sourceMappingURL=index.aa9907df.js.map
